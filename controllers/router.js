127d4c20cb2b60ec8fffba9714cc69b1a5568bb5
jepzekiel <jezedevkiel21@gmail.com>
2022-05-23 19:38:36 +0800

    First Commit

---
+++ controllers/router.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const router = express.Router();
+const db  = require('../config/db-connect');
+
+const { signupValidation, loginValidation } = require('./validation');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', signupValidation, (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE LOWER(email) = LOWER(${db.escape(
+      req.body.email
+    )});`,
+    (err, result) => {
+      if (result.length) {
+        return res.status(409).send({
+          msg: 'Sorry, But Email is already in use 😥'
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).send({
+              msg: err
+            });
+          } else {
+            db.query(
+              `INSERT INTO users (name, email, password, image) VALUES ('${req.body.name}', ${db.escape(req.body.email)}, ${db.escape(hash)}, '${req.body.image}')`,
+              (err, result) => {
+                if (err) {
+                  throw err;
+                  return res.status(400).send({
+                    msg: err
+                  });
+                }
+                return res.status(201).send({
+                  msg: 'User registered Successfully! 👌'
+                });
+              }
+            );
+          }
+        });
+      }
+    }
+  );
+});
+
+router.post('/login', loginValidation, (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE email = ${db.escape(req.body.email)};`,
+    (err, result) => {
+      // user does not exists
+      if (err) {
+        throw err;
+        return res.status(400).send({
+          msg: err
+        });
+      }
+      if (!result.length) {
+        return res.status(401).send({
+          msg: 'Unknown Email!!'
+        });
+      }
+      // check password
+      bcrypt.compare(
+        req.body.password,
+        result[0]['password'],
+        (bErr, bResult) => {
+          // wrong password
+          if (bErr) {
+            throw bErr;
+            return res.status(401).send({
+              msg: 'Password is incorrect!'
+            });
+          }
+          
+          if (bResult) {
+            const token = jwt.sign({id:result[0].id},'the-super-strong-secrect',{ expiresIn: '1h' });
+            db.query(
+              `UPDATE users SET updatedAt = now() WHERE id = '${result[0].id}'`
+            );
+            return res.status(200).send({
+              msg: 'Logged in!',
+              token,
+              user: result[0]
+            });
+          }
+          return res.status(401).send({
+            msg: 'Username or password is incorrect!'
+          });
+        }
+      );
+    }
+  );
+});
+
+router.get('/users', signupValidation, (req, res, next) => {
+    if(
+        !req.headers.authorization ||
+        !req.headers.authorization.startsWith('Bearer') ||
+        !req.headers.authorization.split(' ')[1]
+    ){
+        return res.status(422).json({
+            message: "Please provide the token",
+        });
+    }
+    const theToken = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(theToken, 'the-super-strong-secrect');
+    db.query('SELECT * FROM users where id=?', decoded.id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'Fetch Successfully.' });
+    });
+});
+
+module.exports = router;
